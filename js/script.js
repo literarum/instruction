f3d98ccc4ef434b1baa933a95575e90973168e86
literarum <118631684+literarum@users.noreply.github.com>
2022-12-01 21:54:52 +0400

    Редизайн и реструктура

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@ $('document').ready(function() {
 		$('#tooltip').fadeIn(150).css({
 			'left': e.clientX + -25,
 			'top': e.clientY + -280,
-            'text-align': 'center',
+      		'text-align': 'center',
 			'font-size': '20px',
-			'max-width': '150px',
+			'max-width': '170px',
 			'border-radius': '5px',
-            'border': 'none',
-            'background-color': 'rgba(255, 255, 255, .8)',
+      		'border': 'none',
+      		'background-color': 'rgba(255, 255, 255, .8)',
 			'color': 'rgba(0, 0, 0, .8)',
 			'padding': '12px',
+			'margin-right': '50px',
 			'z-index': '999'
 		});
 		$('#tooltip').show().text(tooltip);
